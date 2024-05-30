8cf4b71c51e338ae167d03c8cc41d84ef84b875d
Elias Bakken <elias@iagent.no>
2024-05-30 16:35:06 +0200

    Use latest tag for KlipperScreen

--- userpatches/overlay/install_components/klipperscreen.sh
+++ userpatches/overlay/install_components/klipperscreen.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+KLIPPERSCREEN_TAG="v0.4.1"
+
 install_klipperscreen() {
     echo "🍰 install KlipperScreen"
     cd /home/debian
     apt install python3.11-venv
     git clone https://github.com/jordanruthe/KlipperScreen.git
+    cd KlipperScreen
+    git checkout tags/$KLIPPERSCREEN_TAG -b $KLIPPERSCREEN_TAG
+    cd ..
     chown -R debian:debian KlipperScreen
     su -c "echo 'Y' | /home/debian/KlipperScreen/scripts/KlipperScreen-install.sh" debian
 }
