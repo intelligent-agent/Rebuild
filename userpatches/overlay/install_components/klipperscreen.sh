46c592ea11567a3e79b5f1cb95c1233d9a9166df
Elias Bakken <elias@iagent.no>
2024-05-31 17:30:48 +0200

    Klipperscreen is fixed

--- userpatches/overlay/install_components/klipperscreen.sh
+++ userpatches/overlay/install_components/klipperscreen.sh
@@ -7,10 +7,6 @@ install_klipperscreen() {
     cd /home/debian
     apt install python3.11-venv
     git clone https://github.com/jordanruthe/KlipperScreen.git
-    cp /tmp/overlay/KlipperScreen/fix-missing-fi.patch /home/debian/KlipperScreen/
-    cd KlipperScreen
-    patch -p1 < fix-missing-fi.patch
-    cd ..
     chown -R debian:debian KlipperScreen
     su -c "echo 'Y' | /home/debian/KlipperScreen/scripts/KlipperScreen-install.sh" debian
 }
