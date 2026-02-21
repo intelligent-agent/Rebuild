e428f4d599b313da2df573a641d21d77424aaf92
Elias Bakken <elias@iagent.no>
2026-02-21 15:52:08 +0100

    Only match webcams with idVendor

--- userpatches/overlay/install_components/ustreamer.sh
+++ userpatches/overlay/install_components/ustreamer.sh
@@ -7,7 +7,7 @@ install_ustreamer() {
     git clone https://github.com/pikvm/ustreamer
     cd ustreamer
     make -j
-    echo 'SUBSYSTEM=="video4linux", ATTR{name}!="cedrus", ATTR{index}=="0", SYMLINK+="webcam", TAG+="systemd"' > /etc/udev/rules.d/50-video.rules
+    echo 'SUBSYSTEM=="video4linux", ATTRS{idVendor}!="", ATTR{index}=="0", SYMLINK+="webcam", TAG+="systemd"' > /etc/udev/rules.d/50-video.rules
     echo '%printer ALL=NOPASSWD: /bin/systemctl restart ustreamer.service' >> /etc/sudoers.d/printer
     cp /tmp/overlay/ustreamer/ustreamer.service /etc/systemd/system/
     chown -R ${USER}:${USER} ${HOMEDIR}/ustreamer
